d020905a3666c872cad3babdc83725cac37457aa
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-01-27 10:38:51 +0100

    feat: added post content

--- src/handlers/post.ts
+++ src/handlers/post.ts
@@ -5,24 +5,25 @@ import dayjs from 'dayjs';
 
 export const createPost: RequestHandler = async (req, res) => {
     try {
-        if (!(req.body?.title)) {
+        if (! (req.body?.title && req.body?.content)) {
             throw new Error('Invalid body provided');
         }
         const post = await db.post.create({
             data: {
                 title: req.body.title,
+                content: req.body.content,
                 authorId: req.user.id,
             }
         });
         if (!post) {
-            return res.status(500).json({ error: "Post list creation failed" });
+            return res.status(500).json({ error: "Post creation failed" });
         }
-        return res.status(201).json({ post })
+        return res.status(201).json({ post });
     } catch (e) {
         console.error({ error: e });
         return res.status(500).json();
     }
-}
+};
 
 export const getAllPosts: RequestHandler = async (req, res) => {
     try {
@@ -52,12 +53,15 @@ export const getAllPosts: RequestHandler = async (req, res) => {
         console.error({ error: e });
         return res.status(500).json();
     }
-}
+};
 
 export const getPostById: RequestHandler = async (req, res) => {
     try {
-        const post = await db.post.findUnique({ where: { id: req.params.id } });
-        if (!post) {
+        const post = await db.post.findUnique({
+            where: { id: req.params.id },
+            include: { comments: true }
+        });
+        if (! post) {
             return res.status(404).json({ error: "Post not found" });
         }
         return res.status(200).json({ post });
@@ -65,7 +69,7 @@ export const getPostById: RequestHandler = async (req, res) => {
         console.error({ error: e });
         return res.status(500).json();
     }
-}
+};
 
 export const deletePost: RequestHandler = async (req, res) => {
     try {
@@ -82,11 +86,11 @@ export const deletePost: RequestHandler = async (req, res) => {
         console.error({ error: e });
         return res.status(500).json();
     }
-}
+};
 
 export const updatePost: RequestHandler = async (req, res) => {
     try {
-        if (! req.body?.title) {
+        if (! (req.body?.title || req.body?.content)) {
             return res.status(400).json({ error: "Invalid body provided" });
         }
         const post = await db.post.findUnique({ where: { id: req.params.id } });
@@ -98,16 +102,17 @@ export const updatePost: RequestHandler = async (req, res) => {
                 id: post.id,
             },
             data: {
-                title: req.body.title
+                title: req.body.title,
+                content: req.body.content,
             }
         });
         if (! updatedPost) {
             return res.status(500).json({ error: "Post update failed" });
         }
-        return res.status(200).json({ message: "Post updated successfully" });
+        return res.status(200).json({ post: updatedPost });
     }
     catch (e) {
         console.error({ error: e });
         return res.status(500).json();
     }
-}
+};
